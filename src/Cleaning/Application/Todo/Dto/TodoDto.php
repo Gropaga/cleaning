65cc1c15554f539be2583f6088e4e016d6fb0031
Maksims Vorobjovs <maksims.vorobjovs@sunfinance.group>
2020-02-17 09:29:52 +0200

    Refactored TodoDto

--- src/Cleaning/Application/Todo/Dto/TodoDto.php
+++ src/Cleaning/Application/Todo/Dto/TodoDto.php
@@ -6,18 +6,9 @@ namespace CleaningCRM\Cleaning\Application\Todo\Dto;
 
 final class TodoDto
 {
-    /** @var string */
-    public ?string $title = null;
-
-    /** @var string */
-    public ?string $description = null;
-
-    /** @var string */
-    public ?string $start = null;
-
-    /** @var string */
-    public ?string $end = null;
-
-    /** @var bool */
-    public ?bool $completed = null;
+    public string $title;
+    public string $description;
+    public string $start;
+    public string $end;
+    public bool $completed;
 }
